98e9e40a49ba09e6142a7bf6ffd95e8177c6e6d0
mdawg <hotdog@america.gob>
2016-10-01 23:58:27 -0400

    add views and root route

--- app/app.php
+++ app/app.php
@@ -2,17 +2,23 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Stylist.php";
 
+    $app = new Silex\Application();
+
     $server = 'mysql:host=localhost:8889;dbname=hair_salon_test';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
-    $app = new Silex\Application();
-
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
 
-    // routes
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+
+
 
     return $app;
+?>
