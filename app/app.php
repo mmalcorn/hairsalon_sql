6f123632e746de1b3485a26de903df947eaff7ea
mmalcorn <alcornmeredith@gmail.com>
2016-10-03 14:56:06 -0400

    add form and twig routes to render all inputted stylists

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
 
     $app = new Silex\Application();
 
@@ -15,6 +16,26 @@
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->get("/stylists/{id}", function($id) use ($app) {
+      $stylist = Stylist::find($id);
+      return $app['twig']->render('stylists.html.twig', array('stylists' => $stylists, 'clients' => $stylist->getClientName()));
+    });
+
+    $app->get("/clients", function() use ($app) {
+      return $app['twig']->render('clients.html.twig', array('clients' => Client::getAll()));
+    });
+
+    $app->post("/stylists", function() use ($app) {
+      $stylist = new Stylist ($_POST['add_stylist']);
+      $stylist->save();
+      return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->post("/remove_stylists", function() use ($app) {
+      Stylist::deleteAll();
+      return $app['twig']->render('index.html.twig');
     });
 
     return $app;
